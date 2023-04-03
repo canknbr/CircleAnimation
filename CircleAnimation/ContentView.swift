532ffaa4ece6735bdaff74fd9852d088a1e713a2
canknbr <cankanbur1@gmail.com>
2023-04-03 03:55:12 +0300

    added sample

--- CircleAnimation/ContentView.swift
+++ CircleAnimation/ContentView.swift
@@ -1,3 +1,4 @@
+
 //
 //  ContentView.swift
 //  CircleAnimation
@@ -7,15 +8,54 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var scaleOut : Bool = false
+    @State var rotateOut : Bool = false
+    @State var moveOut : Bool = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Rectangle().foregroundColor(.black.opacity(0.4)).edgesIgnoringSafeArea(.all)
+            ZStack{
+                
+                //MARK: first
+                ZStack{
+                    Circle().fill(LinearGradient(colors: [.black,.white], startPoint: .top, endPoint: .bottom)).frame(width: 120,height: 120)
+                        .offset(y: moveOut ? -60 : 0)
+                    Circle().fill(LinearGradient(colors: [.black,.white], startPoint: .bottom, endPoint: .top)).frame(width: 120,height: 120)
+                        .offset(y : moveOut ? 60 : 0)
+                }.opacity(0.5)
+                
+                //MARK: second
+                ZStack{
+                    Circle().fill(LinearGradient(colors: [.black,.white], startPoint: .top, endPoint: .bottom)).frame(width: 120,height: 120)
+                        .offset(y: moveOut ? -60 : 0)
+                    Circle().fill(LinearGradient(colors: [.black,.white], startPoint: .bottom, endPoint: .top)).frame(width: 120,height: 120)
+                        .offset(y : moveOut ? 60 : 0)
+                }.opacity(0.5).rotationEffect(.degrees(60))
+                
+                //MARK: third
+                ZStack{
+                    Circle().fill(LinearGradient(colors: [.black,.white], startPoint: .top, endPoint: .bottom)).frame(width: 120,height: 120)
+                        .offset(y: moveOut ? -60 : 0)
+                    Circle().fill(LinearGradient(colors: [.black,.white], startPoint: .bottom, endPoint: .top)).frame(width: 120,height: 120)
+                        .offset(y : moveOut ? 60 : 0)
+                }.opacity(0.5).rotationEffect(.degrees(120))
+                
+                
+                
+            }.rotationEffect(.degrees(rotateOut ? -300 : 0))
+                .scaleEffect(scaleOut ? 1 : 1/4)
+                .animation(Animation.easeInOut.repeatForever(autoreverses: true).speed(0.15))
+                .onAppear{
+                moveOut.toggle()
+                scaleOut.toggle()
+                rotateOut.toggle()
+            }
+            
         }
-        .padding()
     }
 }
 
